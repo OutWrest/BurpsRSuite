eb66390947702ac19d4eaa3c35e897ef125e8dff
Python <salah.abbas142@gmail.com>
2020-05-17 01:19:00 -0500

    Updated readme, added more users for testing

--- BurpsRSuite/Data/BurpsRSuiteInitialization.cs
+++ BurpsRSuite/Data/BurpsRSuiteInitialization.cs
@@ -1,8 +1,11 @@
-﻿using BurpsRSuite.Models;
+﻿using BurpsRSuite.Controllers;
+using BurpsRSuite.Models;
 using Microsoft.AspNetCore.Identity;
+using OtpNet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -10,15 +13,23 @@ namespace BurpsRSuite.Data
 {
     public class BurpsRSuiteInitialization
     {
-        public string test = "test";
         public async static void ResetDatabase(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            // Deletes all past users from previous session
+            // Deletes all past users and roles from previous session
+
             List<Task> Tasks = new List<Task>();
             foreach (ApplicationUser user_to_be_deleted in context.Users)
             {
                 Tasks.Add(userManager.DeleteAsync(user_to_be_deleted));
+            
             }
+
+            foreach (IdentityRole role_to_be_deleted in roleManager.Roles.ToList())
+            {
+                Tasks.Add(roleManager.DeleteAsync(role_to_be_deleted));
+            }
+
+            Task.WaitAll(Tasks.ToArray());
 
             // Create Roles
 
@@ -43,9 +54,7 @@ namespace BurpsRSuite.Data
             Tasks.Add(userManager.CreateAsync(user, "asd").ContinueWith(task =>
                 userManager.AddToRoleAsync(user, AuthorizationRoles.User)));
 
-            Task.WaitAll(Tasks.ToArray());
-
-            // Create admin user
+            // Create a normal admin user
 
             ApplicationUser adminUser = new ApplicationUser()
             {
@@ -57,9 +66,86 @@ namespace BurpsRSuite.Data
                 Answer2 = "a"
             };
 
-            await userManager.CreateAsync(adminUser, "T0tally1337Pa$$");
-            await userManager.AddToRoleAsync(adminUser, AuthorizationRoles.Administrator);
+            Tasks.Add(userManager.CreateAsync(adminUser, "T0tally1337Pa$$").ContinueWith(task =>
+                userManager.AddToRoleAsync(adminUser, AuthorizationRoles.Administrator)));
+
+            Task.WaitAll(Tasks.ToArray());
+
+            // Create a secure user
+
+            Random random = new Random();
+
+            byte[] tfUser = new byte[16];
+            random.NextBytes(tfUser);
+
+
+            ApplicationUser secureUser = new ApplicationUser
+            {
+                UserName = "secureUser",
+                FirstName = "Secure",
+                LastName = "User",
+                Email = "user@protonmail.com",
+                AccountNumber = random.Next(0, 999999999).ToString("000000000"),
+                Answer1 = AdminController.RandomString(5),
+                Answer2 = AdminController.RandomString(5),
+                TotpEnabled = true,
+                TotpSecret = tfUser
+            };
+
+            Tasks.Add(userManager.CreateAsync(secureUser, AdminController.RandomString(5)).ContinueWith(task =>
+                userManager.AddToRoleAsync(secureUser, AuthorizationRoles.User)));
+
+
+            Task.WaitAll(Tasks.ToArray());
+
+            // Create a super secure user
+
+            byte[] tfUser2 = new byte[16];
+            random.NextBytes(tfUser2);
+
+
+            ApplicationUser secureUser2 = new ApplicationUser
+            {
+                UserName = "secureUser" + random.Next(0,99).ToString("00"),
+                FirstName = "VerySecure",
+                LastName = "User",
+                Email = "secureuser@protonmail.com",
+                AccountNumber = random.Next(0, 999999999).ToString("000000000"),
+                Answer1 = AdminController.RandomString(5),
+                Answer2 = AdminController.RandomString(5),
+                TotpEnabled = true,
+                TotpSecret = tfUser
+            };
+
+            Tasks.Add(userManager.CreateAsync(secureUser2, AdminController.RandomString(5)).ContinueWith(task =>
+                userManager.AddToRoleAsync(secureUser2, AuthorizationRoles.User)));
+
+
+            Task.WaitAll(Tasks.ToArray());
+
+            // Create a secure admin user
+
+            byte[] tfAdmin = new byte[16];
+            random.NextBytes(tfAdmin);
+
+            ApplicationUser secureAdmin = new ApplicationUser
+            {
+                UserName = "secureAdmin",
+                FirstName = "Secure",
+                LastName = "Admin",
+                Email = "admin@protonmail.com",
+                AccountNumber = random.Next(0, 999999999).ToString("000000000"),
+                Answer1 = AdminController.RandomString(5),
+                Answer2 = AdminController.RandomString(5),
+                TotpEnabled = true,
+                TotpSecret = tfAdmin
+            };
+
+            Tasks.Add(userManager.CreateAsync(secureAdmin, AdminController.RandomString(5)).ContinueWith(task =>
+                userManager.AddToRoleAsync(secureAdmin, AuthorizationRoles.Administrator)));
+
+            Task.WaitAll(Tasks.ToArray());
         }
-        
+
     }
 }
--- README.md
+++ README.md
@@ -2,12 +2,50 @@
 
 Burps R Suite is a test application made to explore vulnerabilities on ASP.NET web applications.
 
-## Features (TO-DO)
+## Installation 
 
-- [ ] General Website Layout
-- [ ] Different Roles (Admin/Seller/Buyer/User)
-- [ ] Item Database 
-- [ ] Login/Registration
-- [ ] Authentication
-- [ ] Multi-Factor (2FA)
+Use [.NET](https://dotnet.microsoft.com/download) to compile and run the project:
 
+```bash
+git clone https://github.com/OutWrest/BurpsRSuite.git
+cd BurpsRSuite
+dotnet run
+```
+
+The app will run on http://localhost:5000/.
+
+## Testing
+
+There are pre-made test accounts to try out:
+
+*Please note that the password rules are really lax to allow for faster testing.*
+
+| Username     | Password        | First Name | Last Name | Account Number | Answer 1 (Challenge Question) | Answer 2 (Challenge Question) | Has Two Factor |
+|--------------|-----------------|------------|-----------|----------------|-------------------------------|-------------------------------|----------------|
+| user         | asd             | name       | -         | 123            | a                             | a                             | false          |
+| admin        | T0tally1337Pa$$ | name       | -         | 1337           | a                             | a                             | false          |
+| secureUser   | *               | Secure     | User      | **             | *                             | *                             | true           |
+| secureUser** | *               | VerySecure | User      | **             | *                             | *                             | true           |
+| secureAdmin  | *               | Secure     | Admin     | **             | *                             | *                             | true           |
+
+**random lowercase letters (length = 5)*
+
+***random numbers (length = 2 for username and 9 for account number)*
+
+## Known Issues
+
+#### Cookie retention 
+
+When closing a session while logged in, the browser saves the current cookie and loads it again in any new sessions. This causes exceptions because the users are deleted and reset every new session.
+
+##### Fix (not really, just a workaround)
+
+After clicking continue on the exceptions, delete the cookies and refresh the page. 
+
+#### Failure to load users
+
+There is a small chance that only some of the users will be created in a new session. It has something to do with the threads and I have no idea how to fix it.
+
+#### Fix (also, not really)
+
+Restart session and pray.
